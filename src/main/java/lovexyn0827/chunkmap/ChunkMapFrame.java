9ef1dd277a5353e88db7e4ca144a4a47fe8e6291
lovexyn0827 <2717498529@qq.com>
2023-03-04 18:20:59 +0800

    Resizable frame

--- src/main/java/lovexyn0827/chunkmap/ChunkMapFrame.java
+++ src/main/java/lovexyn0827/chunkmap/ChunkMapFrame.java
@@ -3,6 +3,7 @@ package lovexyn0827.chunkmap;
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -55,7 +56,6 @@ import net.minecraft.world.chunk.WorldChunk;
 
 @SuppressWarnings("serial")
 public class ChunkMapFrame extends JFrame {
-
 	private static final ImmutableMap<ChunkHolder.LevelType, Integer> LOADING_LVL_TO_COLOR;
 	private static final ImmutableMap<ChunkTicketType<?>, Integer> TICKET_TO_COLOR;
 	private static final ImmutableMap<LevelType, String> LOADING_LVL_TO_NAME;
@@ -64,13 +64,14 @@ public class ChunkMapFrame extends JFrame {
 	private ServerWorld world;
 	private MinecraftServer server;
 	private Canvas map;
-	private ChunkPos center;
+	private MapArea area;
 	private JLabel status;
 	private JTextArea logArea;
 	private DisplayMode mode = DisplayMode.CHUNK_LOADING;
 	private long lastClick;
 	private boolean renderEntityOverlay;
 	private Map<ChunkPos, ChunkTicket<?>> manuallyLoadedChunks = new HashMap<>();
+	private Object lastDimension;
 
 	public ChunkMapFrame(MinecraftServer server) {
 		super("ChunkMap v20210825--" + server.getSaveProperties().getLevelName());
@@ -86,13 +87,12 @@ public class ChunkMapFrame extends JFrame {
 		this.status.setText("Initializated");
 		this.add(this.status, "South");
 		this.logArea = new JTextArea(80, 30);
-		
 		this.info("The server started");
 		this.logArea.setSize(480, 130);
 		JScrollPane jsp = new JScrollPane(this.logArea);
 		jsp.setSize(480, 130);
 		this.add(jsp, "Center");
-		this.center = new ChunkPos(0, 0);
+		this.area = new MapArea(32, 32, -16, -16);
 		this.map.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -107,19 +107,18 @@ public class ChunkMapFrame extends JFrame {
 		this.map.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent e) {
-				ChunkPos pos = ChunkMapFrame.this.center;
 				switch(e.getKeyChar()) {
 				case 'w':
-					ChunkMapFrame.this.center = new ChunkPos(pos.x, pos.z - (e.isAltDown() ? 16 : 1));
+					ChunkMapFrame.this.moveMap(0, e.isAltDown() ? -16 : -1);
 					break;
 				case 's':
-					ChunkMapFrame.this.center = new ChunkPos(pos.x, pos.z + (e.isAltDown() ? 16 : 1));
+					ChunkMapFrame.this.moveMap(0, e.isAltDown() ? 16 : 1);
 					break;
 				case 'a':
-					ChunkMapFrame.this.center = new ChunkPos(pos.x - (e.isAltDown() ? 16 : 1), pos.z);
+					ChunkMapFrame.this.moveMap(e.isAltDown() ? -16 : -1, 0);
 					break;
 				case 'd':
-					ChunkMapFrame.this.center = new ChunkPos(pos.x + (e.isAltDown() ? 16 : 1), pos.z);
+					ChunkMapFrame.this.moveMap(e.isAltDown() ? 16 : 1, 0);
 					break;
 				case '0':
 					ChunkMapFrame.this.world = ChunkMapFrame.this.server.getWorld(World.NETHER);
@@ -142,7 +141,7 @@ public class ChunkMapFrame extends JFrame {
 					JButton b = new JButton("    OK    ");
 					b.addActionListener((ae) -> {
 						try {
-							ChunkMapFrame.this.center = new ChunkPos(Integer.parseInt(x.getText()), Integer.parseInt(z.getText()));
+							ChunkMapFrame.this.moveCenterTo(Integer.parseInt(x.getText()), Integer.parseInt(z.getText()));
 							dia.setVisible(false);
 						} catch (NumberFormatException ex) {
 						}
@@ -170,6 +169,14 @@ public class ChunkMapFrame extends JFrame {
 		});
 		
 		this.setVisible(true);
+	}
+
+	protected void moveCenterTo(int x, int z) {
+		this.area = this.area.fromCenter(x, z);
+	}
+
+	protected void moveMap(int dx, int dz) {
+		this.area = this.area.move(dx, dz);
 	}
 
 	protected void openHelps() {
@@ -224,8 +231,8 @@ public class ChunkMapFrame extends JFrame {
 	}
 
 	protected void showChunkDetails(int dx, int dz) {
-		int x0 = this.center.x - 15;
-		int z0 = this.center.z - 15;
+		int x0 = this.area.minX;
+		int z0 = this.area.minZ;
 		ServerChunkManager scm = this.world.getChunkManager();
 		ThreadedAnvilChunkStorage tacs = scm.threadedAnvilChunkStorage;
 		ChunkTicketManager ctm = ((ThreadedAnvilChunkStorageMixin)tacs).getTM();
@@ -270,7 +277,7 @@ public class ChunkMapFrame extends JFrame {
 			WorldChunk chunk = ch.getWorldChunk();
 			if(chunk != null) {
 				List<Entity> entities = new ArrayList<>();
-				chunk.collectOtherEntities(null, new Box(new BlockPos(x0 * 16, -65536, z0 * 16)).stretch(480, 10E7, 480), entities, null);
+				chunk.collectOtherEntities(null, new Box(new BlockPos(x0 * 16, -65536, z0 * 16)).stretch(480, 10E4, 480), entities, null);
 				String[][] content = new String[entities.size()][];
 				for(Entity e : entities) {
 					Vec3d p = e.getPos();
@@ -333,125 +340,144 @@ public class ChunkMapFrame extends JFrame {
 	}
 
 	public void tick() {
-		this.setSize(490, 640);
+		this.recalculateSize();
+		int pixelsX = this.area.sizeX * 16;
+		int pixelsZ = this.area.sizeZ * 16;
+		if(pixelsX == 0 || pixelsZ == 0) {
+			return;
+		}
+		
+		Image im = this.map.createImage(pixelsX, pixelsZ);
 		int ticksToAS = 6000 - this.server.getTicks() % 6000;
 		if(ticksToAS == 6000) {
 			this.info("Autosave was performed in tick " + this.world.getTime());
 		}
+		
 		ServerChunkManager scm = this.world.getChunkManager();
 		ThreadedAnvilChunkStorage tacs = scm.threadedAnvilChunkStorage;
 		int loadedCount = scm.getLoadedChunkCount();
-		Image im = this.map.createImage(480, 480);
 		Graphics g = im.getGraphics();
-		int x0 = this.center.x - 15;
-		int z0 = this.center.z - 15;
-		//[-15,14]
-		for(int dx = 0; dx < 30; dx++) {
-			for(int dz = 0; dz < 31; dz ++) {
-				ChunkPos pos = new ChunkPos(x0 + dx, z0 + dz);
-				long posl = pos.toLong();
-				ChunkHolder ch = ((ThreadedAnvilChunkStorageMixin)tacs).getCH(posl);
-				switch(this.mode) {
-				case CHUNK_LOADING:
-					if(ch == null) {
-						g.setColor(Color.WHITE);
-						g.fillRect(dx * 16, dz * 16, 16, 16);
-					} else {
-						g.setColor(new Color(LOADING_LVL_TO_COLOR.get(ChunkHolder.getLevelType(ch.getLevel()))));
-						g.fillRect(dx * 16, dz * 16, dx * 16 + 16, dz * 16 + 16);
-						g.setColor(this.world.isChunkLoaded(pos.x, pos.z) ? Color.BLACK : Color.GRAY);
-						g.drawString(Integer.toString(ch.getLevel()), dx * 16, dz * 16 + 16);
-					}
-					
-					break;
-					
-				case GENERATION:
-					if(ch != null) {
-						Chunk c = ch.getCurrentChunk();
-						if(c != null) {
-							ChunkStatus status = ch.getCurrentChunk().getStatus();
-							if(status != null) {
-								g.setColor(new Color(CHUNK_STATUS_TO_COLOR.get(status)));
-								g.fillRect(dx * 16, dz * 16, 16, 16);
-							}
+		this.area.forEach((x, z, dx, dz) -> {
+			ChunkPos pos = new ChunkPos(x, z);
+			long posl = pos.toLong();
+			ChunkHolder ch = ((ThreadedAnvilChunkStorageMixin)tacs).getCH(posl);
+			switch(this.mode) {
+			case CHUNK_LOADING:
+				if(ch == null) {
+					g.setColor(Color.WHITE);
+					g.fillRect(dx * 16, dz * 16, 16, 16);
+				} else {
+					g.setColor(new Color(LOADING_LVL_TO_COLOR.get(ChunkHolder.getLevelType(ch.getLevel()))));
+					g.fillRect(dx * 16, dz * 16, dx * 16 + 16, dz * 16 + 16);
+					g.setColor(this.world.isChunkLoaded(pos.x, pos.z) ? Color.BLACK : Color.GRAY);
+					g.drawString(Integer.toString(ch.getLevel()), dx * 16, dz * 16 + 16);
+				}
+				
+				break;
+				
+			case GENERATION:
+				if(ch != null) {
+					Chunk c = ch.getCurrentChunk();
+					if(c != null) {
+						ChunkStatus status = ch.getCurrentChunk().getStatus();
+						if(status != null) {
+							g.setColor(new Color(CHUNK_STATUS_TO_COLOR.get(status)));
+							g.fillRect(dx * 16, dz * 16, 16, 16);
 						}
 					}
-					
+				}
+				
+				break;
+			case TICKETS:
+				ChunkTicketManager ctm = ((ThreadedAnvilChunkStorageMixin)tacs).getTM();
+				List<Color> colors = new ArrayList<>();
+				((ChunkTicketManagerMixin)ctm).getTickets()
+						.computeIfAbsent(pos.toLong(), (p) -> SortedArraySet.<ChunkTicket<?>>create(4))
+						.stream()
+						.map(ChunkTicket::getType)
+						.map(TICKET_TO_COLOR::get)
+						.map((in) -> in == null ? 0 : in)
+						.map(Color::new)
+						.filter((c) -> !colors.contains(c))
+						.forEach((colors::add));
+				int[] widths;
+				switch(colors.size()) {
+				default:
+					return;
+				case 1:
+					widths = new int[] {16};
 					break;
-				case TICKETS:
-					ChunkTicketManager ctm = ((ThreadedAnvilChunkStorageMixin)tacs).getTM();
-					List<Color> colors = new ArrayList<>();
-					((ChunkTicketManagerMixin)ctm).getTickets()
-							.computeIfAbsent(pos.toLong(), (p) -> SortedArraySet.<ChunkTicket<?>>create(4))
-							.stream()
-							.map(ChunkTicket::getType)
-							.map(TICKET_TO_COLOR::get)
-							.map((in) -> in == null ? 0 : in)
-							.map(Color::new)
-							.filter((c) -> !colors.contains(c))
-							.forEach((colors::add));
-					int[] widths;
-					switch(colors.size()) {
-					default:
-						continue;
-					case 1:
-						widths = new int[] {16};
-						break;
-					case 2:
-						widths = new int[] {8, 8};
-						break;
-					case 3:
-						widths = new int[] {5, 6, 5};
-						break;
-					case 4:
-						widths = new int[] {4, 4, 4, 4};
-						break;
-					}
-					
-					if(ch != null) {
-						int xOffset = 0;
-						for(int i = 0; i < widths.length; i++) {
-							g.setColor(colors.get(i));
-							g.fillRect(dx * 16 + xOffset, dz * 16, widths[i], 16);
-							xOffset += widths[i];
-						}
-					}
-					
+				case 2:
+					widths = new int[] {8, 8};
+					break;
+				case 3:
+					widths = new int[] {5, 6, 5};
+					break;
+				case 4:
+					widths = new int[] {4, 4, 4, 4};
 					break;
 				}
+				
+				if(ch != null) {
+					int xOffset = 0;
+					for(int i = 0; i < widths.length; i++) {
+						g.setColor(colors.get(i));
+						g.fillRect(dx * 16 + xOffset, dz * 16, widths[i], 16);
+						xOffset += widths[i];
+					}
+				}
+				
+				break;
 			}
-		}
-
+		});
 		g.setColor(Color.BLACK);
-		Vec3d origin = new Vec3d(x0 * 16, 0, z0 * 16);
+		ChunkPos originChunk = this.area.getOriginPos();
+		Vec3d origin = new Vec3d(originChunk.getStartX(), 0, originChunk.getStartZ());
 		if(this.renderEntityOverlay) {
-			for(Entity e : this.world.getOtherEntities(null, new Box(new BlockPos(x0 * 16, -65536, z0 * 16)).stretch(480, 10E7, 480))) {
+			// FIXME Avoid chunk loading
+			for(Entity e : this.world.getOtherEntities(null, new Box(originChunk.getStartPos()).stretch(480, 10E7, 480))) {
 				Vec3d p = e.getPos().subtract(origin);
 				g.fillOval((int)p.x - 3, (int)p.z - 3, 6, 6);
 			}
 		}
 		
-		for(int i = 0; i<= 480; i += 16) {
-			g.drawLine(i, 0, i, 480);
+		for(int i = 0; i<= pixelsX; i += 16) {
+			g.drawLine(i, 0, i, pixelsZ);
 		}
 		
-		for(int i = 0; i<= 480; i += 16) {
-			g.drawLine(0, i, 480, i);
+		for(int i = 0; i<= pixelsZ; i += 16) {
+			g.drawLine(0, i, pixelsX, i);
 		}
 		
 		this.map.getGraphics().drawImage(im, 0, 0, this.map);
 		Point p = this.map.getMousePosition();
 		ChunkPos pos;
 		if(p != null) {
-			pos = new ChunkPos(p.x / 16 + this.center.x - 15, p.y / 16 + this.center.z - 15);
+			pos = new ChunkPos(p.x / 16 + originChunk.x, p.y / 16 + originChunk.z);
 		} else {
-			pos = this.center;
+			pos = originChunk;
 		}
 		
 		String status = this.world.getRegistryKey().getValue() + "   " + pos + "   " + loadedCount + "Chunks Loaded   " + ticksToAS;
 		this.status.setText(status);
 	}
 	
+	private void recalculateSize() {
+		Dimension dim0 = this.getSize();
+		Dimension dim = new Dimension(Math.max(dim0.width, 11), Math.max(dim0.height, 161));
+		if(!dim.equals(this.lastDimension)) {
+			this.lastDimension = dim;
+			this.setSize(dim);
+			int x = (int) Math.ceil((double) Math.max(dim.width - 10, 0) / 16);
+			int z = (int) Math.ceil((double) Math.max(dim.height - 160, 0) / 16);
+			this.area = this.area.resize(x, z);
+			this.logArea.setSize(dim.width - 10, 130);
+			this.map.setSize(dim.width - 10, dim.height - 160);
+			this.validate();
+			this.repaint();
+		}
+	}
+
 	private void info(String string) {
 		this.logArea.append(Instant.now().toString() + " : " + string + '\n');
 	}
